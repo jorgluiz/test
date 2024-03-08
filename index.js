c6a22ece5f0625ce3df9fa8057ff2dbc912c9e77
jorgluiz <jorgeluizti@hotmail.com>
2024-03-08 13:17:20 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+
+// Configuração do CORS para todas as requisições
+app.use(cors());
+
+const { MercadoPagoConfig, Customer, PaymentMethod, CustomerCard, Payment, CardToken } = require('mercadopago');
+const client = new MercadoPagoConfig({ accessToken: 'TEST-5989202427278445-030112-fc76768e07cf6b9c6ea84c4746d4b504-436624597' });
+const customerClient = new Customer(client)
+const customerCard = new CustomerCard(client);
+const payment = new Payment(client);
+const cardToken = new CardToken(client);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'api/views', 'index.html'))
+})
+
+app.post('/process_payment', (req, res) => {
+    console.log(req.body)
+
+    payment.create({
+        body: { 
+            transaction_amount: req.transaction_amount,
+            token: req.token,
+            description: req.description,
+            installments: req.installments,
+            payment_method_id: req.paymentMethodId,
+            issuer_id: req.issuer,
+                payer: {
+                email: req.email,
+                identification: {
+            type: req.identificationType,
+            number: req.number
+        }}},
+        requestOptions: { idempotencyKey: '<SOME_UNIQUE_VALUE>' }
+    })
+    .then((result) => {
+        console.log(result)
+        return res.send(result)
+    })
+    .catch((error) => console.log(error));
+})
+
+app.listen(3000, () => {
+    console.log('Servidor HTTPS está ouvindo na porta 3000');
+});
