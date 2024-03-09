00f21da1ad6a73e184ac2a8821509826b597260d
jorgluiz <jorgeluizti@hotmail.com>
2024-03-09 06:54:22 -0300

    new feature

--- index.js
+++ index.js
@@ -27,35 +27,60 @@ app.use(express.static("./public"));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
 app.post('/process_payment', (req, res) => {
-    console.log(req.body)
+  const { body } = req
+  console.log(body)
 
-    payment.create({
-        body: { 
-            transaction_amount: req.body.transaction_amount,
-            token: req.body.token,
-            description: req.body.description,
-            installments: req.body.installments,
-            payment_method_id: req.body.paymentMethodId,
-            issuer_id: req.body.issuer,
-                payer: {
-                email: req.body.email,
-                identification: {
-            type: req.body.identificationType,
-            number: req.body.number
-        }}},
-        requestOptions: { idempotencyKey: '' }
+ const paymentData = {
+    transaction_amount: Number(body.transaction_amount),
+    token: body.token,
+    description: body.description,
+    installments: Number(body.installments),
+    payment_method_id: body.paymentMethodId,
+    issuer_id: body.issuer,
+    payer: {
+      email: body.email,
+      identification: {
+        type: body.identificationType,
+        number: body.number
+      }
+    }
+  }
+
+  payment.create({ body: paymentData })
+    .then(function (data) {
+      res.status(201).json({
+        detail: data.status_detail,
+        status: data.status,
+        id: data.id,
+      });
     })
-    .then((result) => {
-        console.log(result)
-        return res.send(result)
-    })
-    .catch((error) => console.log(error));
+    .catch(function (error) {
+      console.log(error);
+      const { errorMessage, errorStatus } = validateError(error);
+      res.status(errorStatus).json({ error_message: errorMessage });
+    });
+
 })
 
+function validateError(error) {
+  let errorMessage = "Unknown error cause";
+  let errorStatus = 400;
+
+  if (error.cause) {
+    const sdkErrorMessage = error.cause[0].description;
+    errorMessage = sdkErrorMessage || errorMessage;
+
+    const sdkErrorStatus = error.status;
+    errorStatus = sdkErrorStatus || errorStatus;
+  }
+
+  return { errorMessage, errorStatus };
+}
+
 app.listen(3000, () => {
-    console.log('Servidor HTTPS está ouvindo na porta 3000');
+  console.log('Servidor HTTPS está ouvindo na porta 3000');
 });
